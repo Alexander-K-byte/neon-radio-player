1ac94ad5a1d19db49ac3a859e498ba13113de145
Alexander-K-byte <alexanderkerr@hotmail.com>
2025-09-09 13:18:48 +0300

    initial commit - Neon Radio Player

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import RadioPlayer from "./RadioPlayer";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <RadioPlayer />
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RadioPlayer from "./RadioPlayer";
+import "./RadioPlayer.css"; // make sure your CSS is imported
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RadioPlayer />
+  </React.StrictMode>
+);
